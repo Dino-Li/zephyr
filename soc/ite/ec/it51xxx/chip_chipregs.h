5282318e4a33c9cb7c9ae606416af3793e4c77a0
Tim Lin <tim2.lin@ite.corp-partner.google.com>
2024-10-22 18:40:54 +0800

    soc: Separate it8xxx2 related files from the common folder
    Some files should be separated from the common folder to
    each soc folder for future expansion of the chip.
    
    Change-Id: I44a1d422f7549b29076c3dbd62a0e9f7925b0714
    Signed-off-by: Tim Lin <tim2.lin@ite.corp-partner.google.com>

---
+++ soc/ite/ec/it51xxx/chip_chipregs.h
@@ -0,0 +1,445 @@
+/*
+ * Copyright (c) 2024 ITE Corporation. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef CHIP_CHIPREGS_H
+#define CHIP_CHIPREGS_H
+
+#include <zephyr/sys/util.h>
+
+#ifdef _ASMLANGUAGE
+#define ECREG(x) x
+#else
+
+/*
+ * Macros for hardware registers access.
+ */
+#define ECREG(x)     (*((volatile unsigned char *)(x)))
+#define ECREG_u16(x) (*((volatile unsigned short *)(x)))
+#define ECREG_u32(x) (*((volatile unsigned long *)(x)))
+
+#endif /* _ASMLANGUAGE */
+
+/**
+ *
+ * (1Cxxh) SMBus Interface (SMB) registers
+ *
+ */
+#define IT51XXX_SMB_BASE     0x00F01C00
+#define IT51XXX_SMB_SADFPCTL ECREG(IT51XXX_SMB_BASE + 0x55)
+/* 0x55: Slave A FIFO Control */
+#define IT51XXX_SMB_HSAPE    BIT(1)
+
+/**
+ *
+ * (10xxh) Shared Memory Flash Interface Bridge (SMFI) registers
+ *
+ */
+#ifndef __ASSEMBLER__
+struct smfi_it51xxx_regs {
+	volatile uint8_t reserved1[59];
+	/* 0x3B: EC-Indirect memory address 0 */
+	volatile uint8_t SMFI_ECINDAR0;
+	/* 0x3C: EC-Indirect memory address 1 */
+	volatile uint8_t SMFI_ECINDAR1;
+	/* 0x3D: EC-Indirect memory address 2 */
+	volatile uint8_t SMFI_ECINDAR2;
+	/* 0x3E: EC-Indirect memory address 3 */
+	volatile uint8_t SMFI_ECINDAR3;
+	/* 0x3F: EC-Indirect memory data */
+	volatile uint8_t SMFI_ECINDDR;
+	/* 0x40: Scratch SRAM 0 address low byte */
+	volatile uint8_t SMFI_SCAR0L;
+	/* 0x41: Scratch SRAM 0 address middle byte */
+	volatile uint8_t SMFI_SCAR0M;
+	/* 0x42: Scratch SRAM 0 address high byte */
+	volatile uint8_t SMFI_SCAR0H;
+	volatile uint8_t reserved1_1[23];
+	/* 0x5A: Host RAM Window Control */
+	volatile uint8_t SMFI_HRAMWC;
+	/* 0x5B: Host RAM Window 0 Base Address [11:4] */
+	volatile uint8_t SMFI_HRAMW0BA;
+	/* 0x5C: Host RAM Window 1 Base Address [11:4] */
+	volatile uint8_t SMFI_HRAMW1BA;
+	/* 0x5D: Host RAM Window 0 Access Allow Size */
+	volatile uint8_t SMFI_HRAMW0AAS;
+	/* 0x5E: Host RAM Window 1 Access Allow Size */
+	volatile uint8_t SMFI_HRAMW1AAS;
+	volatile uint8_t reserved_5f_80[34];
+	/* 0x81: Flash control 6 */
+	volatile uint8_t SMFI_FLHCTRL6R;
+};
+#endif /* !__ASSEMBLER__ */
+
+/* SMFI register fields */
+/* EC-Indirect read internal flash */
+#define EC_INDIRECT_READ_INTERNAL_FLASH BIT(6)
+/* Enable EC-indirect page program command */
+#define IT51XXX_SMFI_MASK_ECINDPP       BIT(3)
+#define IT8XXX2_SMFI_MASK_ECINDPP       IT51XXX_SMFI_MASK_ECINDPP
+/* 0x42: Scratch SRAM 0 address high byte */
+#define SCARH_ENABLE                    BIT(7)
+#define SCARH_ADDR_BIT19                BIT(3)
+
+/**
+ *
+ * (11xxh) Interrupt controller (INTC)
+ *
+ */
+#ifndef __ASSEMBLER__
+struct intc_it51xxx_regs {
+	/* 0x00-0x0f: Reserved_00_0f */
+	volatile uint8_t reserved_00_0f[16];
+	/* 0x10: Interrupt Vector */
+	volatile uint8_t INTC_VECT;
+};
+#endif /* !__ASSEMBLER__ */
+
+/**
+ *
+ * (16xxh) General Purpose I/O Port (GPIO) registers
+ *
+ */
+#define GPIO_IT51XXX_REGS_BASE ((struct gpio_it51xxx_regs *)DT_REG_ADDR(DT_NODELABEL(gpiogcr)))
+
+#ifndef __ASSEMBLER__
+struct gpio_it51xxx_regs {
+	/* 0x00: General Control */
+	volatile uint8_t GPIO_GCR;
+	/* 0x01-D0: Reserved1 */
+	volatile uint8_t reserved1[208];
+	/* 0xD1: General Control 25 */
+	volatile uint8_t GPIO_GCR25;
+	/* 0xD2: General Control 26 */
+	volatile uint8_t GPIO_GCR26;
+	/* 0xD3: General Control 27 */
+	volatile uint8_t GPIO_GCR27;
+	/* 0xD4: General Control 28 */
+	volatile uint8_t GPIO_GCR28;
+	/* 0xD5: General Control 31 */
+	volatile uint8_t GPIO_GCR31;
+	/* 0xD6: General Control 32 */
+	volatile uint8_t GPIO_GCR32;
+	/* 0xD7: General Control 33 */
+	volatile uint8_t GPIO_GCR33;
+	/* 0xD8-0xDF: Reserved2 */
+	volatile uint8_t reserved2[8];
+	/* 0xE0: General Control 16 */
+	volatile uint8_t GPIO_GCR16;
+	/* 0xE1: General Control 17 */
+	volatile uint8_t GPIO_GCR17;
+	/* 0xE2: General Control 18 */
+	volatile uint8_t GPIO_GCR18;
+	/* 0xE3: Reserved3 */
+	volatile uint8_t reserved3;
+	/* 0xE4: General Control 19 */
+	volatile uint8_t GPIO_GCR19;
+	/* 0xE5: General Control 20 */
+	volatile uint8_t GPIO_GCR20;
+	/* 0xE6: General Control 21 */
+	volatile uint8_t GPIO_GCR21;
+	/* 0xE7: General Control 22 */
+	volatile uint8_t GPIO_GCR22;
+	/* 0xE8: General Control 23 */
+	volatile uint8_t GPIO_GCR23;
+	/* 0xE9: General Control 24 */
+	volatile uint8_t GPIO_GCR24;
+	/* 0xEA-0xEC: Reserved4 */
+	volatile uint8_t reserved4[3];
+	/* 0xED: General Control 30 */
+	volatile uint8_t GPIO_GCR30;
+	/* 0xEE: General Control 29 */
+	volatile uint8_t GPIO_GCR29;
+	/* 0xEF: Reserved5 */
+	volatile uint8_t reserved5;
+	/* 0xF0: General Control 1 */
+	volatile uint8_t GPIO_GCR1;
+	/* 0xF1: General Control 2 */
+	volatile uint8_t GPIO_GCR2;
+	/* 0xF2: General Control 3 */
+	volatile uint8_t GPIO_GCR3;
+	/* 0xF3: General Control 4 */
+	volatile uint8_t GPIO_GCR4;
+	/* 0xF4: General Control 5 */
+	volatile uint8_t GPIO_GCR5;
+	/* 0xF5: General Control 6 */
+	volatile uint8_t GPIO_GCR6;
+	/* 0xF6: General Control 7 */
+	volatile uint8_t GPIO_GCR7;
+	/* 0xF7: General Control 8 */
+	volatile uint8_t GPIO_GCR8;
+	/* 0xF8: General Control 9 */
+	volatile uint8_t GPIO_GCR9;
+	/* 0xF9: General Control 10 */
+	volatile uint8_t GPIO_GCR10;
+	/* 0xFA: General Control 11 */
+	volatile uint8_t GPIO_GCR11;
+	/* 0xFB: General Control 12 */
+	volatile uint8_t GPIO_GCR12;
+	/* 0xFC: General Control 13 */
+	volatile uint8_t GPIO_GCR13;
+	/* 0xFD: General Control 14 */
+	volatile uint8_t GPIO_GCR14;
+	/* 0xFE: General Control 15 */
+	volatile uint8_t GPIO_GCR15;
+	/* 0xFF: Power Good Watch Control */
+	volatile uint8_t GPIO_PGWCR;
+};
+#endif /* !__ASSEMBLER__ */
+
+/* GPIO register fields */
+/* 0x00: General Control */
+#define IT51XXX_GPIO_LPCRSTEN             (BIT(2) | BIT(1))
+#define IT8XXX2_GPIO_LPCRSTEN             IT51XXX_GPIO_LPCRSTEN
+#define IT51XXX_GPIO_GCR_ESPI_RST_D2      0x2
+#define IT51XXX_GPIO_GCR_ESPI_RST_POS     1
+#define IT51XXX_GPIO_GCR_ESPI_RST_EN_MASK (0x3 << IT51XXX_GPIO_GCR_ESPI_RST_POS)
+/* 0xF0: General Control 1 */
+#define IT51XXX_GPIO_U2CTRL_SIN1_SOUT1_EN BIT(2)
+#define IT51XXX_GPIO_U1CTRL_SIN0_SOUT0_EN BIT(0)
+/* 0xE6: General Control 21 */
+#define IT51XXX_GPIO_GPH1VS               BIT(1)
+#define IT51XXX_GPIO_GPH2VS               BIT(0)
+
+#define KSIX_KSOX_KBS_GPIO_MODE BIT(7)
+#define KSIX_KSOX_GPIO_OUTPUT   BIT(6)
+#define KSIX_KSOX_GPIO_PULLUP   BIT(2)
+#define KSIX_KSOX_GPIO_PULLDOWN BIT(1)
+
+#define GPCR_PORT_PIN_MODE_INPUT    BIT(7)
+#define GPCR_PORT_PIN_MODE_OUTPUT   BIT(6)
+#define GPCR_PORT_PIN_MODE_PULLUP   BIT(2)
+#define GPCR_PORT_PIN_MODE_PULLDOWN BIT(1)
+
+/*
+ * If both PULLUP and PULLDOWN are set to 1b, the corresponding port would be
+ * configured as tri-state.
+ */
+#define GPCR_PORT_PIN_MODE_TRISTATE                                                                \
+	(GPCR_PORT_PIN_MODE_INPUT | GPCR_PORT_PIN_MODE_PULLUP | GPCR_PORT_PIN_MODE_PULLDOWN)
+
+/**
+ *
+ * (1Exxh) Clock and Power Management (ECPM) registers
+ *
+ */
+#define ECPM_IT51XXX_REGS_BASE ((struct ecpm_it51xxx_regs *)DT_REG_ADDR(DT_NODELABEL(ecpm)))
+
+#ifndef __ASSEMBLER__
+struct ecpm_it51xxx_regs {
+	/* 0x00: Reserved1 */
+	volatile uint8_t reserved1;
+	/* 0x01: Clock Gating Control 1 */
+	volatile uint8_t CGCTRL1R;
+	/* 0x02: Clock Gating Control 2 */
+	volatile uint8_t CGCTRL2R;
+	/* 0x03: PLL Clock */
+	volatile uint8_t PLLCTRL;
+	/* 0x04: Auto Clock Gating */
+	volatile uint8_t AUTOCG;
+	/* 0x05: Clock Gating Control 3 */
+	volatile uint8_t CGCTRL3R;
+	/* 0x06: PLL Frequency */
+	volatile uint8_t PLLFREQR;
+	/* 0x07: PLL SSC Control */
+	volatile uint8_t PLLSSCR;
+	/* 0x08: Reserved2 */
+	volatile uint8_t reserved2;
+	/* 0x09: Clock Gating Control 4 */
+	volatile uint8_t CGCTRL4R;
+};
+#endif /* !__ASSEMBLER__ */
+
+/* ECPM register fields */
+/* 0x02: CLOCK Gating Control 2 */
+#define IT51XXX_ECPM_CIRCG    BIT(5)
+#define IT51XXX_ECPM_SWUCCG   BIT(4)
+/* 0x04: Auto CLOCK Gating */
+#define IT51XXX_ECPM_AUART1CG BIT(6)
+#define IT51XXX_ECPM_AUART2CG BIT(5)
+#define IT51XXX_ECPM_ASSPICG  BIT(4)
+#define IT51XXX_ECPM_ACIRCG   BIT(2)
+/* 0x05: CLOCK Gating Control 3 */
+#define IT51XXX_ECPM_PECICG   BIT(3)
+#define IT51XXX_ECPM_UART12CG BIT(2)
+#define IT51XXX_ECPM_SSPICG   BIT(1)
+#define IT51XXX_ECPM_DBGRCG   BIT(0)
+/* 0x05: CLOCK Gating Control 4 */
+#define IT51XXX_ECPM_CEC1CG   BIT(1)
+#define IT51XXX_ECPM_CEC0CG   BIT(0)
+
+#ifndef __ASSEMBLER__
+enum chip_pll_mode {
+	CHIP_PLL_DOZE = 0,
+	CHIP_PLL_SLEEP = 1,
+	CHIP_PLL_DEEP_DOZE = 3,
+};
+#endif
+
+/**
+ *
+ * (1Fxxh) External Timer & External Watchdog (ETWD)
+ *
+ */
+#define IT51XXX_EXT_TIMER_BASE ((struct timer_it51xxx_regs *)DT_REG_ADDR(DT_NODELABEL(timer)))
+
+#ifndef __ASSEMBLER__
+struct timer_it51xxx_regs {
+	/* 0x00: External Timer Configuration 2 */
+	volatile uint8_t ETWCFG2;
+	/* 0x01-0x04: Reserved_01_04 */
+	volatile uint8_t reserved_01_04[4];
+	/* 0x05: External Control */
+	volatile uint8_t ETWCTRL;
+	/* 0x06-0x09: Reserved_06_09 */
+	volatile uint8_t reserved_06_09[4];
+	/* 0x0a: External Timer 2 Prescaler */
+	volatile uint8_t ET2PSR;
+	/* 0x0b: External Timer 2 Counter High Byte */
+	volatile uint8_t ET2CNTLHR;
+	/* 0x0c: External Timer 2 Counter Low Byte */
+	volatile uint8_t ET2CNTLLR;
+	/* 0x0d: reserved_0d */
+	volatile uint8_t reserved_0d[1];
+	/* 0x0e: External Timer 2 Counter High Byte 2 */
+	volatile uint8_t ET2CNTLH2R;
+	/* 0x0f: reserved_0f */
+	volatile uint8_t reserved_0f[1];
+	/* 0x10: External Timer 3 Prescaler */
+	volatile uint8_t ET3PSR;
+	/* 0x11: External Timer 3 Counter High Byte */
+	volatile uint8_t ET3CNTLHR;
+	/* 0x12: External Timer 3 Counter Low Byte */
+	volatile uint8_t ET3CNTLLR;
+	/* 0x13: External Timer 3 Counter High Byte 2 */
+	volatile uint8_t ET3CNTLH2R;
+	/* 0x14: External Timer 4 Prescaler */
+	volatile uint8_t ET4PSR;
+	/* 0x15: reserved_15 */
+	volatile uint8_t reserved_15[1];
+	/* 0x16: External Timer 4 Counter Low Byte */
+	volatile uint8_t ET4CNTLLR;
+	/* 0x17-0x20: Reserved_17_20 */
+	volatile uint8_t reserved_17_20[10];
+	/* 0x21: External Timer Terminal Count Write Clear */
+	volatile uint8_t ETTMLCNTWCR;
+};
+
+enum ext_clk_src_sel {
+	EXT_PSR_32P768K = 0,
+	EXT_PSR_1P024K,
+	EXT_PSR_32,
+	EXT_PSR_EC_CLK,
+};
+/*
+ * 24-bit timers: external timer 2
+ * 24-bit timers: external timer 3
+ */
+enum ext_timer_idx {
+	EXT_TIMER_2 = 0, /* Event timer */
+	EXT_TIMER_3,     /* Busy wait timer */
+};
+#endif /* __ASSEMBLER__ */
+
+/* ETWD register fields */
+/* 0x05: External Control */
+#define IT51XXX_ETWD_ET3TC   BIT(7)
+#define IT51XXX_ETWD_ET3RST  BIT(7)
+#define IT51XXX_ETWD_ET2RST  BIT(2)
+/* 0x21: External Timer Terminal count write clear */
+#define IT51XXX_ETWD_ET3TCWC BIT(2)
+
+/**
+ *
+ * (20xxh) General Control (GCTRL) registers
+ *
+ */
+#define GCTRL_IT51XXX_REGS_BASE ((struct gctrl_it51xxx_regs *)DT_REG_ADDR(DT_NODELABEL(gctrl)))
+
+#ifndef __ASSEMBLER__
+struct gctrl_it51xxx_regs {
+	/* 0x00-0x01: Reserved_00_01 */
+	volatile uint8_t reserved_00_01[2];
+	/* 0x02: Chip Version */
+	volatile uint8_t GCTRL_ECHIPVER;
+	/* 0x03-0x05: Reserved_03_05 */
+	volatile uint8_t reserved_03_05[3];
+	/* 0x06: Reset Status */
+	volatile uint8_t GCTRL_RSTS;
+	/* 0x07-0x09: Reserved_07_09 */
+	volatile uint8_t reserved_07_09[3];
+	/* 0x0A: Base Address Select */
+	volatile uint8_t GCTRL_BADRSEL;
+	/* 0x0B: Wait Next Clock Rising */
+	volatile uint8_t GCTRL_WNCKR;
+	/* 0x0C: Special Control 5 */
+	volatile uint8_t GCTRL_SPCTRL5;
+	/* 0x0D: Special Control 1 */
+	volatile uint8_t GCTRL_SPCTRL1;
+	/* 0x0E-0x0F: reserved_0e_0f */
+	volatile uint8_t reserved_0e_0f[2];
+	/* 0x10: Reset Control DMM */
+	volatile uint8_t GCTRL_RSTDMMC;
+	/* 0x11: Reset Control 4 */
+	volatile uint8_t GCTRL_RSTC4;
+	/* 0x12-0x1B: reserved_12_1b */
+	volatile uint8_t reserved_12_1b[10];
+	/* 0x1C: Special Control 4 */
+	volatile uint8_t GCTRL_SPCTRL4;
+	/* 0x1D-0x1F: reserved_1d_1f */
+	volatile uint8_t reserved_1d_1f[3];
+	/* 0x20: Reset Control 5 */
+	volatile uint8_t GCTRL_RSTC5;
+	/* 0x21-0x37: reserved_21_37 */
+	volatile uint8_t reserved_21_37[23];
+	/* 0x38: Special Control 9 */
+	volatile uint8_t GCTRL_SPCTRL9;
+	/* 0x39-0x46: reserved_39_46 */
+	volatile uint8_t reserved_39_46[14];
+	/* 0x47: Scratch SRAM0 Base Address */
+	volatile uint8_t GCTRL_SCR0BAR;
+	/* 0x48: Scratch ROM 0 Size */
+	volatile uint8_t GCTRL_SCR0SZR;
+	/* 0x49-0x84: reserved_49_84 */
+	volatile uint8_t reserved_49_84[60];
+	/* 0x85: Chip ID Byte 1 */
+	volatile uint8_t GCTRL_ECHIPID1;
+	/* 0x86: Chip ID Byte 2 */
+	volatile uint8_t GCTRL_ECHIPID2;
+	/* 0x87: Chip ID Byte 3 */
+	volatile uint8_t GCTRL_ECHIPID3;
+};
+#endif /* !__ASSEMBLER__ */
+
+/* GCTRL register fields */
+/* 0x06: Reset Status */
+#define IT51XXX_GCTRL_LRS         (BIT(1) | BIT(0))
+#define IT51XXX_GCTRL_IWDTR       BIT(1)
+/* 0x0B: Wait Next 65K Rising */
+#define IT51XXX_GCTRL_WN65K       0x00
+/* 0x10: Reset Control DMM */
+#define IT51XXX_GCTRL_UART1SD     BIT(3)
+#define IT51XXX_GCTRL_UART2SD     BIT(2)
+/* 0x11: Reset Control 4 */
+#define IT51XXX_GCTRL_RPECI       BIT(4)
+#define IT51XXX_GCTRL_RUART       BIT(2)
+/* 0x1C: Special Control 4 */
+#define IT51XXX_GCTRL_LRSIWR      BIT(2)
+#define IT51XXX_GCTRL_LRSIPWRSWTR BIT(1)
+#define IT51XXX_GCTRL_LRSIPGWR    BIT(0)
+/* 0x38: Special Control 9 */
+#define IT51XXX_GCTRL_ALTIE       BIT(4)
+/* 0x48: Scratch ROM 0 Size */
+#define IT51XXX_GCTRL_SCRSIZE_4K  0x03
+
+/* Alias gpio_it8xxx2_regs to gpio_it51xxx_regs for compatibility */
+#define gpio_it8xxx2_regs       gpio_it51xxx_regs
+/* Alias smfi_it8xxx2_regs to smfi_it51xxx_regs for compatibility */
+#define smfi_it8xxx2_regs       smfi_it51xxx_regs
+/* Alias gctrl_it8xxx2_regs to gctrl_it51xxx_regs for compatibility */
+#define gctrl_it8xxx2_regs      gctrl_it51xxx_regs
+#define GCTRL_IT8XXX2_REGS_BASE GCTRL_IT51XXX_REGS_BASE
+
+#endif /* CHIP_CHIPREGS_H */
